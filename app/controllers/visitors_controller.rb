bee3cf79eaa6b3804840032aa0596e5ecbf5f6d1
Nam Tran <namtran0110@gmail.com>
2017-03-01 20:49:15 +0700

    milestone 4

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,5 +1,10 @@
 class VisitorsController < ApplicationController
   def index
-    @products = Product.all
+    if params[:sort] == "alphabetical"
+      @products = Product.alphabetical
+    else
+      @products = Product.all
+    end
   end
+
 end
